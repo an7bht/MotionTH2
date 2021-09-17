f398f4d9c3d192cadf45ea8e59deacee6be43db4
AnDang <81478990+an7bht@users.noreply.github.com>
2021-09-17 19:24:06 +0700

    lan 1

---
+++ app/src/main/java/com/example/motionth2/Motion2Activity.java
@@ -0,0 +1,14 @@
+package com.example.motionth2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Motion2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_motion2);
+    }
+}
